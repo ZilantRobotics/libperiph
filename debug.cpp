a12ca132884db77816584572d7e1ccecfcfce620
Dima <PonomarevDA96@gmail.com>
2018-07-17 17:17:06 +0300

    DebugPort: some additions + overload Transim(): now it can work with null-terminated string + add templates

--- debug.cpp
+++ debug.cpp
@@ -1,6 +1,6 @@
 /**
 * @file debug.cpp
-* @brief Implementation of debug
+* @brief Implementation of Debug Port
 */
 
 #include <uart.hpp>
@@ -11,20 +11,51 @@ extern UART UART1;
 DebugPort Debug;
 
 
+/**
+* @brief Init debug port
+*/
 void DebugPort::Init()
 {
 	UART1.Init(UART_1);
 }
 
 
-void DebugPort::Transmit(const uint8_t* arr, const uint8_t& length)
+/**
+* @brief Transmit array of data to Debug Port
+* @param arr - pointer to array
+* @param length - length of array
+*/
+template <class T>
+void DebugPort::Transmit(const T* arr, const uint8_t& length)
 {
-	UART1.SendArr(arr, length);
+	UART1.TransmitArr(arr, length);
 }
+template void DebugPort::Transmit(const uint8_t* arr, const uint8_t& length);
+template void DebugPort::Transmit(const char* arr, const uint8_t& length);
 
 
-
-void DebugPort::Receive(uint8_t* arr, uint8_t& length)
+/**
+* @brief Transmit null-terminated string to Debug Port
+* @param str - pointer to null-terminated string
+*/
+template <class T>
+void DebugPort::Transmit(const T* str)
 {
-	UART1.GetData(arr, length);
+	UART1.TransmitString(str);
 }
+template void DebugPort::Transmit(const uint8_t* arr);
+template void DebugPort::Transmit(const char* arr);
+
+
+/**
+* @brief Receive array of data from Debug Port
+* @param arr - pointer to array
+* @param length - length of array
+*/
+template <class T>
+void DebugPort::Receive(T* arr, uint8_t& length)
+{
+	UART1.ReceiveArr(arr, length);
+}
+template void DebugPort::Receive(uint8_t* arr, uint8_t& length);
+template void DebugPort::Receive(char* arr, uint8_t& length);
--- debug.hpp
+++ debug.hpp
@@ -1,3 +1,8 @@
+/**
+* @file debug.hpp
+* @brief Class of DebugPort
+*/
+
 #ifndef __DEBUG_HPP
 #define __DEBUG_HPP
 
@@ -8,8 +13,15 @@ class DebugPort
 	public:
 		DebugPort(): IsItInit(DEBUG_IS_NOT_INITIALIZED) {};
 		void Init();
-		void Transmit(const uint8_t* arr, const uint8_t& length);
-		void Receive(uint8_t* arr, uint8_t& length);
+			
+		template <class T>
+		void Transmit(const T* arr, const uint8_t& length);
+		
+		template <class T>
+		void Transmit(const T* str);
+		
+		template <class T>
+		void Receive(T* arr, uint8_t& length);
 	private:
 		enum: uint8_t 
 		{
