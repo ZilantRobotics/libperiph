dfdd1e2ab8d0d9f324587eb2ffbe1b5934953cc3
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-10-08 23:51:55 +0300

    add license to adc

--- platform_specific/stm32/adc/adc.cpp
+++ platform_specific/stm32/adc/adc.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2018-2022 Dmitry Ponomarev <ponomarevda96@gmail.com>
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 /**
  * @file adc.cpp
  * @author d.ponomarev
