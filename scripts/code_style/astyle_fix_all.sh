8dd0bae432c72d5564bc890199848605f921ecb1
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-11-14 19:45:55 +0300

    improve code style checker

--- scripts/code_style/astyle_fix_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Check http://astyle.sourceforge.net/astyle.html
- 
-
-ASTYLE_SCRIPT_PATH=$(dirname $(readlink -f $0))/astyle_fix_single_file.sh
-WS_DIR=$(dirname $(readlink -f $0))/../..
-cd $WS_DIR
