43531c4b8fe45d953fe020a09fca6526abda1e4c
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-11-14 19:45:55 +0300

    improve code style checker

--- scripts/code_style/cpplint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# It should be called from can_node folder
-cpplint sensors/*/* \
-        common/* \
-        devices/*/* \
-        devices/* \
-        platform_specific/* \
-        platform_specific/*/* \
-        tests/*/*
