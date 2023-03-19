e9ccc38ba9798f27d715e7e4686e535ee59307ef
Dmitry Ponomarev <ponomarevda96@gmail.com>
2023-03-19 21:25:17 +0300

    code style fixes

--- sensors/gps/ublox_commands.c
+++ sensors/gps/ublox_commands.c
@@ -8,12 +8,18 @@
 #include "libperiph_common.h"
 #include "hal_uart.h"
 
+#define FACTORY_RESET_SIZE      21
+#define PROFILE_0_SIZE          28
+#define NAVPVT_DISABLE_SIZE     26
+#define NAVPVT_ENABLE_SIZE      26
+#define UBLOX_RATE_10_SIZE      22
+
+
 /***
  *  @brief: uBlox configuration: Restore the factory configuration
  *
  *  PS. each uBlox Package starts with two bytes 0xB5, 0x62  and ends with checksum
  *      refer to user manual.
- *  * 
  * @note  the naming snakeCase convention is as follows:
  * - static  uint8_t <Protocol><PacketType><ContentProfile> [] ={bytes,...};
  *  e.g,
@@ -21,12 +27,12 @@
  *       protocol -> uBlox
  *       PacketType -> Port
  *       ContentProfile -> Poll
- * 
  */
 static uint8_t uBloxConfigFactoryReset[] = {
     0xB5, 0x62, 0x06, 0x09, 0x0D, 0x00, 0xFF, 0xFF, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x03, 0x1B,
-    0x9A};
+    0x9A
+};
 /***
  * @brief: sets the Port configuration as following:
  * Target : UART 1
@@ -37,12 +43,14 @@ static uint8_t uBloxConfigFactoryReset[] = {
 static uint8_t uBloxPortProfile0[] = {
     0xB5, 0x62, 0x06, 0x00, 0x14, 0x00, 0x01, 0x00, 0x00, 0x00,
     0xD0, 0x08, 0x00, 0x00, 0x00, 0xC2, 0x01, 0x00, 0x07, 0x00,
-    0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0xC0, 0x7E};
+    0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0xC0, 0x7E
+};
 /***
  *  @brief: polls for current Port configuration on uBlox Module
  */
 static uint8_t uBloxPortPoll[] = {
-    0xB5, 0x62, 0x06, 0x00, 0x01, 0x00, 0x01, 0x08, 0x22};
+    0xB5, 0x62, 0x06, 0x00, 0x01, 0x00, 0x01, 0x08, 0x22
+};
 
 /***
  *  @brief: configures the GPS rate to 10Hz
@@ -52,7 +60,8 @@ static uint8_t uBloxPortPoll[] = {
 static uint8_t uBloxRates10Hz[] = {
     0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0x64, 0x00, 0x01, 0x00,
     0x01, 0x00, 0x7A, 0x12, 0xB5, 0x62, 0x06, 0x08, 0x00, 0x00,
-    0x0E, 0x30};
+    0x0E, 0x30
+};
 
 /***
  *  @brief: configures Messages 01-07 NAV-PVT on UART 1
@@ -61,62 +70,53 @@ static uint8_t uBloxRates10Hz[] = {
 static uint8_t uBloxMessagesNavPvtEnable[] = {
     0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x07, 0x00, 0x01,
     0x00, 0x00, 0x00, 0x00, 0x18, 0xE1, 0xB5, 0x62, 0x06, 0x01,
-    0x02, 0x00, 0x01, 0x07, 0x11, 0x3A};
+    0x02, 0x00, 0x01, 0x07, 0x11, 0x3A
+};
 
 /***
  *  @brief: deconfigures Messages 01-07 NAV-PVT on UART 1
  *  PS. this command contains two pakcages MSG+CFG
  */
-static uint8_t uBloxMessagesNavPvtDisable[] = {
+static uint8_t uBloxMessagesNavPvtDisable[NAVPVT_DISABLE_SIZE] = {
     0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x07, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00, 0x17, 0xDC, 0xB5, 0x62, 0x06, 0x01,
-    0x02, 0x00, 0x01, 0x07, 0x11, 0x3A};
+    0x02, 0x00, 0x01, 0x07, 0x11, 0x3A
+};
 
 /**
  * @brief configures the ublox interface
  * @param DMA boolean flag to send configuration by DMA or Polling mode
- *
  */
-uint8_t ubloxConfigure(bool DMA)
-{
-
+uint8_t ubloxConfigure(bool DMA) {
     int8_t (*Transmit)(uint8_t[], size_t) = DMA ? &uartTransmitDma : &uartTransmit;
 
     UartChangeBaudrate(115200);
-    if (Transmit(uBloxConfigFactoryReset,
-                 sizeof(uBloxConfigFactoryReset) / sizeof(*uBloxConfigFactoryReset)) == STATUS_ERROR)
-    {
+
+    if (Transmit(uBloxConfigFactoryReset, FACTORY_RESET_SIZE) == STATUS_ERROR) {
         return STATUS_ERROR;
     };
 
     UartChangeBaudrate(9600);
-    if (Transmit(uBloxConfigFactoryReset,
-                 sizeof(uBloxConfigFactoryReset) / sizeof(*uBloxConfigFactoryReset)) == STATUS_ERROR)
-    {
+
+    if (Transmit(uBloxConfigFactoryReset, FACTORY_RESET_SIZE) == STATUS_ERROR) {
         return STATUS_ERROR;
     };
-    if (Transmit(uBloxPortProfile0,
-                 sizeof(uBloxPortProfile0) / sizeof(*uBloxPortProfile0)) == STATUS_ERROR)
-    {
+
+    if (Transmit(uBloxPortProfile0, PROFILE_0_SIZE) == STATUS_ERROR) {
         return STATUS_ERROR;
     };
 
     UartChangeBaudrate(115200);
-    if (Transmit(uBloxMessagesNavPvtDisable,
-                 sizeof(uBloxMessagesNavPvtDisable) / sizeof(*uBloxMessagesNavPvtDisable)) == STATUS_ERROR)
-    {
+
+    if (Transmit(uBloxMessagesNavPvtDisable, NAVPVT_DISABLE_SIZE) == STATUS_ERROR) {
         return STATUS_ERROR;
     };
 
-    if (Transmit(uBloxRates10Hz,
-                 sizeof(uBloxRates10Hz) / sizeof(*uBloxRates10Hz)) == STATUS_ERROR)
-    {
+    if (Transmit(uBloxRates10Hz, UBLOX_RATE_10_SIZE) == STATUS_ERROR) {
         return STATUS_ERROR;
     };
 
-    if (Transmit(uBloxMessagesNavPvtEnable,
-                 sizeof(uBloxMessagesNavPvtEnable) / sizeof(*uBloxMessagesNavPvtEnable)) == STATUS_ERROR)
-    {
+    if (Transmit(uBloxMessagesNavPvtEnable, NAVPVT_ENABLE_SIZE) == STATUS_ERROR) {
         return STATUS_ERROR;
     };
 
