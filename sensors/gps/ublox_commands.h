40fec20038a6eac25f435c2480a1433ac2f1a94b
Dmitry Ponomarev <36133264+PonomarevDA@users.noreply.github.com>
2023-05-29 15:10:56 +0300

    improve ublox configuration (#29)

--- sensors/gps/ublox_commands.h
+++ sensors/gps/ublox_commands.h
@@ -5,23 +5,52 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-/**
- * @file ublox_commands.h
- * @author ehsan shaghaei
- * @date Dec 16, 2022
- * @brief This header file contains uBlox commands.
- */
-
 #ifndef SENSORS_GPS_UBLOX_COMMANDS_H
 #define SENSORS_GPS_UBLOX_COMMANDS_H
 
 #include <stdint.h>
-#include <stdbool.h>
+#include <stddef.h>
+
+typedef enum {
+    UBX_CMD_FACTORY_RESET,
+    UBX_CMD_BAUDRATE_921600,
+
+    UBX_SET_BAUDRATE_9600,
+    UBX_SET_BAUDRATE_115200,
+    UBX_SET_BAUDRATE_921600,
+
+    UBX_MON_HW,
+    UBX_NAV_COV,
+    UBX_NAV_PVT,
+    UBX_NAV_STATUS,
+    UBX_TIM_TIM2,
+
+    UBX_CMD_RATE_10_HZ,
+    UBX_CMD_SAVE_CONFIG,
+} UbloxCommand;
+
+typedef int8_t (*UbxTransmit_t)(uint8_t[], size_t);
+typedef void (*UbxDelay_t)(uint32_t);
+typedef void (*UbxChangeBaudRate_t)(uint32_t);
+
 
 /**
- * @brief configures the ublox interface
- * @param DMA boolean flag to send configuration by DMA or Polling mode
+ * @brief Initialize the module. You have to provide 3 functions pointers.
+ * @param transmit - either DMA or not
+ * @param delay - either os related or blocking
+ * @param changeBaudRate - required to be able to configure the gnss on any baudrate
  */
-uint8_t ubloxConfigure(bool DMA);
+int8_t ubloxInit(UbxTransmit_t transmit, UbxDelay_t delay, UbxChangeBaudRate_t changeBaudRate);
+
+/**
+ * @brief Configure U-Blox gnss receiver
+ */
+int8_t ubloxConfigure();
+
+
+/**
+ * @brief Execute a specific command
+ */
+int8_t ubloxExecuteCommand(UbloxCommand command);
 
 #endif  // SENSORS_GPS_UBLOX_COMMANDS_H
